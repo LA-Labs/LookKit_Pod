9fc916ec56e0f949fcacb008b7767c8e715554f2
amir-lahav <amir.lahav@shutterfly.com>
2021-02-28 15:57:44 +0200

    main

---
+++ LookKit.xcframework/ios-arm64_x86_64-simulator/LookKit.framework/Headers/LookKit.h
@@ -0,0 +1,18 @@
+//
+//  LookKit.h
+//  LookKit
+//
+//  Created by Amir Lahav on 22/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LookKit.
+FOUNDATION_EXPORT double LookKitVersionNumber;
+
+//! Project version string for LookKit.
+FOUNDATION_EXPORT const unsigned char LookKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LookKit/PublicHeader.h>
+
+
